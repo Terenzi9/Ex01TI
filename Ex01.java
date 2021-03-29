2abcfe7afcfb3d4dc8f64b12c453d6fc02ad7fdf
Terenzi9 <65312437+Terenzi9@users.noreply.github.com>
2021-03-28 22:24:14 -0300

    Create Ex01.java

---
+++ Ex01.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+ class Ex01 {
+	
+	 public static void main(String args[]) {
+	 public static Scanner sc = new Scanner(System.in);
+	 
+	 int num1,num2,soma;
+	 
+	 System.out.println("Digite um numero");
+	 num1 = sc.nextlnt();
+	 System.out.println("Digite outro numero");
+	 num2 = sc.nextlnt();
+	 
+	 soma = num1+num2;
+	 
+	 System.out.println("Soma"+soma);
+	 }
+}
